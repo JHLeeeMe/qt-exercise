fb82ffe3172c4fbb019e85f51a747fa649d4025e
JHLeeeMe <lejung92@gmail.com>
2023-03-23 12:01:56 +0900

    [Add] Hello, Qt!
    	새 파일:       01_HelloWorld/01_HelloWorld.pro
    	새 파일:       01_HelloWorld/hello-world.cpp

---
+++ 01_HelloWorld/hello-world.cpp
@@ -0,0 +1,11 @@
+#include <QtCore/QCoreApplication>
+#include <QtCore/QDebug>
+
+int main(int argc, char* argv[])
+{
+    QCoreApplication app{ argc, argv };
+
+    qDebug() << "Hello, Qt!";
+
+    return app.exec();
+}
