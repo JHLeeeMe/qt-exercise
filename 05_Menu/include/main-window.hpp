65a034286a31ed6280919746216fc7701689ba1a
JHLeeeMe <lejung92@gmail.com>
2023-03-25 18:04:06 +0900

    [Add] Menu
    	새 파일:       05_Menu/05_Menu.pro
    	새 파일:       05_Menu/include/main-window.hpp
    	새 파일:       05_Menu/main-window.cpp
    	새 파일:       05_Menu/main.cpp

---
+++ 05_Menu/include/main-window.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QMenu>
+#include <QtWidgets/QMenuBar>
+
+class MainWindow : public QMainWindow
+{
+public:
+    MainWindow(QWidget* parent = nullptr);
+};
+
